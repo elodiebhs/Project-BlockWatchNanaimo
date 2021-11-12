203b6fa86c76f3214056da5b5c9296fae7354d32
elodiebhs <elodiebouthors@hotmail.com>
2021-11-12 14:10:06 -0800

    import route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 
 import Todo from './Components/Todo'
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div >
+
+
+
       <h1>My Todos</h1>
       <Todo text="Learn React"/>
     </div>
