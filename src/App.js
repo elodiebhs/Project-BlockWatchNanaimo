89e67ba040e56f40764fe42694762f817fa165ed
elodiebhs <elodiebouthors@hotmail.com>
2021-11-12 14:19:14 -0800

    import components and create routes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@
 
 import Todo from './Components/Todo'
 import { Route } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups"
+import NewMeetupPage from "./pages/NewMeetup"
+import FavoriteMeetupPage from "./pages/FavoriteMeetup"
+
 
 function App() {
   return (
     <div >
+      <Route path="/">
+        <AllMeetupsPage />
+      </Route>
 
+      <Route path="/new-meetup">
+        <NewMeetupPage/>
+      </Route>
+
+      <Route path="/favorites">
+        <FavoriteMeetupPage />
+      </Route>
 
 
       <h1>My Todos</h1>
-      <Todo text="Learn React"/>
+      <Todo text="Learn React" />
     </div>
   );
 }
