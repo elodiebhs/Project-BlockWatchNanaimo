dc1c95f1f0ea1442e9c025c2565d5063d25c92e5
elodiebhs <elodiebouthors@hotmail.com>
2021-11-12 15:19:43 -0800

    add css to navigation

--- src/Components/layout/MainNavigation.js
+++ src/Components/layout/MainNavigation.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import classes from './MainNavigation.module.css';
 
 function MainNavigation() {
   return (
-    <header>
-      <div>
+    <header className={classes.header}>
+      <div className={classes.logo}>
         Logo
      </div>
 
