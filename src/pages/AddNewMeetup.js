baf22664a76a6bd1e3b76d47da4ba5a579d2b477
elodiebhs <elodiebouthors@hotmail.com>
2021-11-13 15:57:39 -0800

    navigating to home page after submitting form new meetup

--- src/pages/AddNewMeetup.js
+++ src/pages/AddNewMeetup.js
@@ -1,18 +1,24 @@
-import React from 'react'
-import NewMeetupForm from '../Components/meetups/NewMeetupForm'
+import React from 'react';
+import {useHistory} from 'react-router-dom';
+import NewMeetupForm from '../Components/meetups/NewMeetupForm';
 
 
 function AddNewMeetupPage() {
+  //gives use an history object
+  const history = useHistory();
+
   function addMeetupHandler(meetupData){
     //get the url from FireBase
     //by default fecth send a get request
-    fetch('https://blockwatch-88cf6-default-rtdb.firebaseio.com/meetups.json',{
+    fetch('https://blockwatch-88cf6-default-rtdb.firebaseio.com/meetups.json',
+    {
       method: 'POST',
       body:JSON.stringify(meetupData),
       headers:{
         "content-Type":'application/json'
       }
-    });
+    }
+    ).then(()=>{history.replace('/')})
   }
   return (
    <section>
