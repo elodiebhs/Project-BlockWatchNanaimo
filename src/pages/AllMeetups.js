8d35460751c198cae445ab54b449e46d14e414f2
elodiebhs <elodiebouthors@hotmail.com>
2021-11-13 16:44:50 -0800

    transform data

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -35,27 +35,36 @@ function AllMeetupsPage() {
     ).then((response) => {
       return response.json();
     }).then((data) => {
+      const meetups = [];
+      for (const key in data) {
+        const meetup = {
+          id: key,
+          ...data[key]
+        }
+
+        meetups.push(meetup)
+      }
       //we set loading to false when we have data
       setIsLoading(false)
-      setLoadedMeetups(data)
+      setLoadedMeetups(meetups)
     });
   }, []);
 
   if (isLoading) {
     return (<section><p>Loading...</p></section>);
-}
-return (
-  <section>
-    <h1>All Meetups</h1>
-    {/* <ul>
+  }
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      {/* <ul>
       {DUMMY_DATA.map((meetup)=>{
         return <li key={meetup.id}>{meetup.title}</li>
       })}
       </ul> */}
-    {/* <MeetupList meetups={DUMMY_DATA} /> */}
-    <MeetupList meetups={loadedMeetups} />
-  </section>
-);
+      {/* <MeetupList meetups={DUMMY_DATA} /> */}
+      <MeetupList meetups={loadedMeetups} />
+    </section>
+  );
 }
 
 export default AllMeetupsPage
